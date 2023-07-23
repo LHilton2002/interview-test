ad6212622f8e69e36a2fa28edff5fb9d3c6c788d
Wxrlockz <72805699+Wxrlockz@users.noreply.github.com>
2023-07-23 23:47:44 +0100

    Logout commit
    added logout functionality
    removes token from local storage and takes the user back to login page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,6 +7,13 @@ import { profileData } from "@/app/api/profile/route";
 import Head from "next/head";
 
 import "./profile.css";
+import "styles/globals.css";
+
+
+const handleLogout = () => {
+  localStorage.removeItem("token");
+  window.location.href = "/";
+}
 
 const ProfilePage = () => {
   useAuth();
@@ -32,11 +39,30 @@ const ProfilePage = () => {
     fetchProfile();
   }, []);
 
+  const userPosts = [
+    {
+      id: 1,
+      imageUrl: "https://picsum.photos/300",
+      description: "placeholder user post 1",
+    },
+    {
+      id: 2,
+      imageUrl: "https://picsum.photos/300",
+      description: "placeholder user post 2",
+    },
+    {
+      id: 3,
+      imageUrl: "https://picsum.photos/300",
+      description: "placeholder user post 3",
+    },
+  ]
+
   return (
-    <section className="header">
+    <section>
       <Head>
         <title>Profile Page</title>
       </Head>
+      <header className="header">
       <div className="container">
         <div className="profile">
           <img
@@ -49,6 +75,19 @@ const ProfilePage = () => {
             <ul className="profile-details">{profileData.email}</ul>
             <p className="profile-bio">{profileData.bio}</p>
           </div>
+        </div>
+      </div>
+      <button className="fixed top-0 left-0 p-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={handleLogout}>Logout</button>
+      </header>
+
+      <div className="fixed top-15 left-5 container mt-8">
+        <div className="grid grid-cols-3 gap-4">
+          {userPosts.map((post) => (
+            <div key={post.id} className="bg-white shadow-md rounded-lg p-4">
+              <img className="post-image" src={post.imageUrl} alt="post" />
+              <p className="post-description">{post.description}</p>
+      </div>
+          ))}
         </div>
       </div>
     </section>
